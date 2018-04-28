ec8ae1f038f10a69c0d2838ec0e82af8074ee191
Valink16 <valuthyk@gmail.com>
2018-04-28 17:14:08 +0200

    Added swear system

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const Discord = require('discord.js');
 Public.botClient = new Discord.Client();
 
 //Events
-Public.botClient.on('message', Bot.replyCmd);
+Public.botClient.on('message', Bot.reponseCmd);
 
-Bot.insulter();
+console.log(Bot.choisirInsulte("GENERAL"));
 
 var promise = Public.botClient.login('NDM1NTA2ODAzMjA4NjE4MDA1.DbaBzA.c-0jLAuHkMmZAnoEKVYs5m33QVs');
 
