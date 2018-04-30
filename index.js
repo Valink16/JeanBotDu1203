ebe894204705bbb545868b9573ad7c9849433238
Valink16 <valuthyk@gmail.com>
2018-04-30 22:27:10 +0200

    Some fixes + added better error system

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const Public = require('public');
 const Fs = require('fs');
 const Bot = require('bot');
 const Discord = require('discord.js');
-const token =''
+const token = 'NDM1NTA2ODAzMjA4NjE4MDA1.DckIYw.jJhvar38mOdC1-ABWLRRa7cY8Wg';
 Public.botClient = new Discord.Client();
 
 //Events
 Public.botClient.on('message', Bot.reponseCmd);
-
-console.log(Bot.choisirInsulte("GENERAL"));
 
 var promise = Public.botClient.login(token);
